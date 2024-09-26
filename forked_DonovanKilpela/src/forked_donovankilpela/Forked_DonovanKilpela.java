b1aad2e54040882c1d3fa4417c70c19b5c0e0f20
Donovan Kilpela <161644962+DonovanKilpela@users.noreply.github.com>
2024-09-25 22:57:59 -0500

    Adding a Java file that will output my name when Ran

---
+++ forked_DonovanKilpela/src/forked_donovankilpela/Forked_DonovanKilpela.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package forked_donovankilpela;
+
+/**
+ *
+ * @author donov
+ */
+public class Forked_DonovanKilpela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Adding my Name Donovan Kilpela");
+        
+    }
+    
+}
